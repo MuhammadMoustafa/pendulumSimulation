d91879278826eeba8ecc92e117069545a6a00d37
MuhammadMoustafa <engmuhammadmoustafa@gmail.com>
2019-07-30 13:08:34 +0200

    add damping coeff

--- pendulum.js
+++ pendulum.js
@@ -1,22 +1,24 @@
 class Pendulum {
 
-    constructor(origin, length) {
+    constructor(origin, radius) {
 
         this.origin = origin;
-        this.length = length;
-        this.bob = createVector(origin.x, length);
+        this.radius = radius;
+        this.bob = createVector(origin.x, radius);
         this.angle = PI / 4;
-        this.radius = 50;
+        this.pendRadius = 50;
         this.aAcc = 0;
         this.aVel = 0;
-        this.g = -0.0001;
+        this.g = -10;
+        this.mass = 500;
+        this.fricCoeff = 10000;
     }
 
     show() {
         stroke(255);
         strokeWeight(3);
         line(this.origin.x, this.origin.y, this.bob.x, this.bob.y);
-        ellipse(this.bob.x, this.bob.y, this.radius, this.radius);
+        ellipse(this.bob.x, this.bob.y, this.pendRadius, this.pendRadius);
     }
 
     go() {
@@ -25,9 +27,9 @@ class Pendulum {
     }
 
     update() {
-        this.bob.x = this.origin.x + this.length * sin(this.angle);
-        this.bob.y = this.origin.y + this.length * cos(this.angle);
-        this.aAcc = this.g * sin(this.angle);
+        this.bob.x = this.origin.x + this.radius * sin(this.angle);
+        this.bob.y = this.origin.y + this.radius * cos(this.angle);
+        this.aAcc = -this.fricCoeff / (this.mass * this.radius ** 2) * this.aVel + this.g * sin(this.angle) / this.radius;
         this.aVel += this.aAcc;
         this.angle += this.aVel;
     }
@@ -36,6 +38,7 @@ class Pendulum {
         this.aVel = 0;
         this.aAcc = 0;
         this.angle = PI / 2 - this.origin.angleBetween(createVector(x - this.origin.x, this.origin.y - y));
+        this.radius = dist(this.origin.x, this.origin.y, x, y);
         fill(100)
         stroke(150);
     }
--- phasePlane.js
+++ phasePlane.js
@@ -2,6 +2,9 @@ class PhasePlane {
 
     constructor(origin) {
         this.origin = origin;
+        this.diag = [];
+        this.stablePoints = [];
+        this.scale = 1000;
     }
 
 
@@ -12,8 +15,35 @@ class PhasePlane {
         translate(this.origin);
         line(-500, 0, 500, 0); // x axis
         line(0, -200, 0, 200); // y axis
-        ellipse(p.aVel * 20000, p.aAcc * 20000, 10, 10);
-        console.log(p.aVel * 20000, p.aAcc * 20000);
+
+        let elem = {
+            vel: p.aVel,
+            acc: p.aAcc
+        };
+        this.diag.push(elem);
+        noFill();
+
+        beginShape();
+        this.diag.forEach(element => {
+            vertex(element.vel * this.scale, element.acc * this.scale);
+        });
+        endShape()
+
+        stroke(100, 125, 255);
+        ellipse(elem.vel * this.scale, elem.acc * this.scale, 10, 10);
+
+
+        if (abs(elem.vel) < 0.01 && abs(elem.acc) < 0.01) {
+            console.log("eeeeeeeeeeeeeeeeeeeeeh");
+            this.stablePoints.push(elem);
+        } else {
+            console.log(abs(elem.vel), abs(elem.acc), abs(sin(p.angle)));
+        }
+
+        stroke(255, 125, 100);
+        this.stablePoints.forEach(element => {
+            point(element.vel * this.scale, element.acc * this.scale);
+        })
         pop();
     }
 }
